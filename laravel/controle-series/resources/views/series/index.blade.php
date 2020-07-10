88b56a7ea32a07eae2be4c41e41315ed45023e30
gustavogranja <gustavo.cisco235@gmail.com>
2020-07-10 19:31:12 -0300

    Laravel aula 4

--- laravel/controle-series/resources/views/series/index.blade.php
+++ laravel/controle-series/resources/views/series/index.blade.php
@@ -17,12 +17,29 @@ Séries
         <ul class="list-group">
             @foreach ($series as $serie)
             <li class="list-group-item d-flex justify-content-between align-items-center">
-                {{ $serie->nome }}
+                <span id="nome-serie-{{$serie->id}}">{{$serie->nome}}</span>
+
+                <div class="input-group w-50" hidden id="input-nome-serie-{{$serie->id}}">
+
+                    <input type="text" class="form-control" value="{{$serie->nome}}">
+
+                    <div class="input-group-append">
+                        <button class="btn btn-primary" onclick="editarSerie({{$serie->id}})">
+                            <i class="fas fa-check"></i>
+                        </button>
+                        @csrf
+                    </div>
+                </div>
 
                 <span class="d-flex">
+                    <button class="btn btn-secondary btn-sm mr-2" onclick="toggleInput({{$serie->id}})">
+                        <i class="fas fa-edit"></i>
+                    </button>
+
                     <a href="/series/{{$serie->id}}/temporadas" class="btn btn-info btn-sm mr-2">
                         <i class="fas fa-external-link-alt"></i>
                     </a>
+
                     <form method="post" action="/series/{{$serie->id}}"
                         onsubmit="return confirm('Tem certeza?')">
                         @csrf
@@ -35,4 +52,40 @@ Séries
             </li>
             @endforeach
          </ul>
+        <script>
+            function toggleInput(serieId) {
+                const nomeSerieEl = document.getElementById(`nome-serie-${serieId}`);
+                const inputSerieEl = document.getElementById(`input-nome-serie-${serieId}`);
+                if (nomeSerieEl.hasAttribute('hidden')) {
+                    nomeSerieEl.removeAttribute('hidden');
+                    inputSerieEl.hidden = true;
+                } else {
+                    inputSerieEl.removeAttribute('hidden');
+                    nomeSerieEl.hidden = true;
+                }
+
+            }
+
+            function editarSerie(serieId) {
+                let formData = new FormData();
+
+                const nome = document
+                    .querySelector(`#input-nome-serie-${serieId} > input`)
+                    .value;
+                const token = document.querySelector('input[name="_token"]').value;
+
+                formData.append('nome', nome);
+                formData.append('_token', token);
+
+                const url = `/series/${serieId}/editaNome`;
+                fetch(url, {
+                   body: formData,
+                    method: 'POST'
+                }).then(() => {
+                    toggleInput(serieId);
+                    document.getElementById(`nome-serie-${serieId}`).textContent = nome;
+                });
+
+            }
+        </script>
 @endsection
