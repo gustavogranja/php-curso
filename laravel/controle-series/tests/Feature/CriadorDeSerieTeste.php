b0bec2bad692351849201f2b6b590cc6fc62b851
gustavogranja <gustavo.cisco235@gmail.com>
2020-07-14 14:45:17 -0300

    Laravel aula 8 prt1

---
+++ laravel/controle-series/tests/Feature/CriadorDeSerieTeste.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Serie;
+use App\Services\CriadorDeSerie;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CriadorDeSerieTeste extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testCriarSerie()
+    {
+        $criadorDeSerie = new CriadorDeSerie();
+        $nomeSerie = 'Nome de teste';
+        $serieCriada = $criadorDeSerie->criarSerie($nomeSerie, 1, 1);
+
+       // $this->assertInstanceOf(Serie::class, $serieCriada);*/
+       // $this->assertDatabaseHas('series', ['nome' => $nomeSerie]);
+       // $this->assertDatabaseHas('temporadas', ['serie_id' => $serieCriada->id, 'numero' => 1]);
+       // $this->assertDatabaseHas('episodios', ['numero' => 1]);
+    }
+
+
+}
